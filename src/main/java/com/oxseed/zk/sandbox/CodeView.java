2b664d16bbe2281614ed5e71780cd917ed2b6e30
vpupkin <vpupkin@localhost>
2010-08-04 10:13:15 +0200

    rhino ++

--- src/main/java/com/oxseed/zk/sandbox/CodeView.java
+++ src/main/java/com/oxseed/zk/sandbox/CodeView.java
@@ -1,15 +1,15 @@
-package com.oxseed.zk.sandbox;
-
-import java.util.Map;
-
-import net.sf.jsr107cache.Cache;
-
+package com.oxseed.zk.sandbox; 
+import net.sf.jsr107cache.Cache; 
 import org.apache.commons.logging.LogFactory;
 import org.zkoss.zk.ui.*;
 import org.zkoss.zk.ui.ext.AfterCompose;
 import org.zkoss.zul.*;
  
 public class CodeView extends Textbox implements AfterCompose {
+	/**
+	 * @author vipup
+	 */
+	private static final long serialVersionUID = 559317910798406915L;
 	private static org.apache.commons.logging.Log log = LogFactory.getLog(""+CodeView.class);
 	public void afterCompose() {
 		execute();
@@ -20,8 +20,7 @@ public class CodeView extends Textbox implements AfterCompose {
 		Cache cache = CodeCache.getCache();//Map attributes = session.getAttributes();
 		String valueTmp = (String)cache.get(DemoWindow.ZK_DEMO_CONTENT); 
 		valueTmp = valueTmp ==null? getValue():valueTmp;
-		log.debug(valueTmp);  
-		cache.put(DemoWindow.ZK_DEMO_CONTENT, valueTmp);//attributes.put("zk.demo.content" ,valueTmp );
+		log.debug(valueTmp);   
 		Executions.createComponentsDirectly(valueTmp, "zul", view, null);
 	}
 }
