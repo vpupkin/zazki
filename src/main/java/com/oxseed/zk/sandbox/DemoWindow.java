253fb2191743e08addaa08ddbae0e0d9f7cba086
vpupkin <vpupkin@localhost>
2010-08-09 17:44:07 +0200

    5.0.3 chk Ok wit captcha
    http://localhost:9090/zazki/hello.zul
    
     <window title="My First Window" border="normal" width="500px">
    
    <grid fixedLayout="true">
    	<columns>
    		<column label="Type" width="150px" />
    		<column label="Content" />
    	</columns>
    	<rows>
    		<row>
    			<label value="UserName" width="150px" />
    			<textbox value="Jerry" width="150px" />
    		</row>
    		<row>
    			Password
    			<textbox type="password" value="123456789" width="150px" />
    		</row>
    		<row>
    			Re-type Password
    			<textbox type="password" value="123456789" width="150px" />
    		</row>
    		<row>
    			Age:
    			<intbox value="19" constraint="no negative,no zero"
    				width="150px" />
    		</row>
    		<row>
    			Phone:
    			<intbox constraint="no negative,no zero" width="150px"
    				value="12345678" />
    		</row>
    		<row>
    			Weight:
    			<decimalbox format="###.##" value="154.32" width="150px" />
    		</row>
    		<row>
    			Birthday:
    			<hbox>
    				<datebox id="db" width="150px" />
    				<listbox
    					onSelect="db.setFormat(self.selectedItem.value)" mold="select"
    					rows="1">
    					<listitem label="Default" value="" />
    					<listitem label="yyyy/MM/dd hh:mm a" value="yyyy/MM/dd hh:mm a" />
    					<listitem label="yyyy/MM/dd" value="yyyy/MM/dd" />
    					<listitem label="MM-dd-yy" value="MM-dd-yy" />
    				</listbox>
    			</hbox>
    		</row>
    		<row>
    			Address
    			<vbox>
    				<textbox width="250px"
    					value="123 W. 45 Street, New York, NY 10001" />
    				<hbox>
    					<label value="Zip code :" />
    					<intbox constraint="no negative,no zero"
    						width="150px" />
    				</hbox>
    			</vbox>
    		</row>
    		<row>
    			E-mail:
    			<textbox width="150px" value="zk@zkoss.org"
    				constraint="/.+@.+\.[a-z]+/: Please enter an e-mail address" />
    		</row>
    		<row>
    			Introduction:
    			<hbox>
    				<textbox id="intro" rows="3" width="250px">
    					<attribute name="value">I think ZK is the best! </attribute>
    				</textbox>
    				<vbox>
    					More line :
    						<spinner value="3" constraint="no negative,no zero"
    							onChanging="if (event.value.length() > 0 &amp;&amp; Integer.parseInt(event.value) > 0) intro.rows = Integer.parseInt(event.value);" />
    				</vbox>
    			</hbox>
    		</row>
    		<row>
    			Validation
    			<hbox>
    				<vbox>
    					<span sclass="edition-field">
    						Captcha and Colorbox requires
    						<a href="http://www.zkoss.org/download/zk.dsp" sclass="edition-field" label=" ZK PE or EE."/>
    					</span>
    					<textbox value="qwasdf" width="150px" onChange="cpa.value = self.value"/>
    					<captcha id="cpa" length="6" width="150px"
    						height="50px" />
    				</vbox>
    				<vbox>
    					<label value=" " pre="true"/>
    					(Case Insensitive)
    					<button label="Regenerate"
    						onClick="cpa.randomValue();" />
    
    				</vbox>
    			</hbox>
    		</row>
    	</rows>
    </grid>
    
    	sdafHello, World!			</window>

--- src/main/java/com/oxseed/zk/sandbox/DemoWindow.java
+++ src/main/java/com/oxseed/zk/sandbox/DemoWindow.java
@@ -11,8 +11,9 @@ public class DemoWindow extends Window {
 
 	public void onCreate() {
 		final Include inc = new Include();
-		inc.setSrc("/barDemo.zul"); //inc.getSrc()
-		Component componentTmp = Path.getComponent("/demo/codeView");//demo/tryView/
+		inc.setSrc("/barDemo.zul"); //inc.getSrc() // "/welcome page/secondContent/demo" @/welcome page/secondContent/demo/tryView@
+		// for index.zul == welcome page/secondContent/demo/codeView
+		Component componentTmp =   Path.getComponent("/demo/codeView") ; //.getChildren().toArray()[1]) ;// Path. getPath(  (org.zkoss.zul.impl.XulElement)   (( (org.zkoss.zul.impl.XulElement)Path.getComponent("/").getChildren().toArray()[1]).getChildren().toArray()[0]) ) FellowIfAny( "demo").  demo/codeView");//demo/tryView/
 		Textbox tbTmp = (Textbox)componentTmp;
 		String valueTmp = ValueVersioningManager.checkOut(ZK_DEMO_CONTENT);
 		
