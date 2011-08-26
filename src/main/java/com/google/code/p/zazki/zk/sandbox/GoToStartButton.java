a5da2de0d9c52e5453a4db8c5f9723b9bedc26e4
vasiliij pupkin <vasiliij.pupkin@gmail.com>
2011-08-26 16:12:18 +0200

    NPE Fix for Tomcat6.0 deploy !!! http://localhost:8080/zazki/hello.zul

---
+++ src/main/java/com/google/code/p/zazki/zk/sandbox/GoToStartButton.java
@@ -0,0 +1,19 @@
+package com.google.code.p.zazki.zk.sandbox; 
+import org.zkoss.zk.ui.*;
+import org.zkoss.zul.*;
+
+/**
+ * The "Try me!" button.
+ * 
+ * @author tomyeh
+ */
+public class GoToStartButton extends Button {
+	public void onClick() {
+		Component componentTmp = getFellow("codeView"); 
+		Textbox tbTmp = (Textbox)componentTmp;	
+		String valueTmp = tbTmp.getValue();
+		Component ZKError = Path.getComponent("/welcome"); 
+		Components.removeAllChildren(ZKError);	
+		Executions.createComponentsDirectly(valueTmp, "zul", ZKError, null);
+	}
+}
